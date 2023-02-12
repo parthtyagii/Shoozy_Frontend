e53fd15adf2cc08d5b1f44c9a352e05d21d2e827
Parth Tyagi <parthtyagicode@gmail.com>
2023-02-12 17:59:22 +0530

    customer delivery information box

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -45,13 +45,59 @@ function CheckoutPage() {
 
                 </div>
 
-                {/* ----------------------------------------------------------------------------------- */}
+
 
                 <div className="deliveryInformation">
+
+                    <h2>Delivery Information</h2>
+
+                    <div className="customerName">
+                        <div class='customerInfo'>
+                            <label>First Name *</label>
+                            <input type="text" name='firstName' placeholder='Type here...' />
+                        </div>
+
+                        <div class='customerInfo'>
+                            <label>Last Name *</label>
+                            <input type="text" name='lastName' placeholder='Type here...' />
+                        </div>
+                    </div>
+
+                    <div className="address customerInfo">
+                        <label>Address *</label>
+                        <input type="text" name="address" />
+                    </div>
+
+                    <div className="city">
+                        <div class='customerInfo'>
+                            <label>City / Town</label>
+                            <input type="text" name="city" placeholder='Type here...' />
+                        </div>
+
+                        <div class='customerInfo'>
+                            <label>Zip Code *</label>
+                            <input type="text" name="zip" placeholder='Type here...' />
+                        </div>
+                    </div>
+
+                    <div className="customerId">
+                        <div class='customerInfo'>
+                            <label>Mobile</label>
+                            <input type="text" name="mobile" placeholder='Type here...' />
+                        </div>
+
+                        <div class='customerInfo'>
+                            <label>Email *</label>
+                            <input type="text" name="email" placeholder='Type here...' />
+                        </div>
+                    </div>
 
                 </div>
 
             </div>
+
+
+            {/* --------------------------------------------------------------------------------- */}
 
             <div className="checkoutPageRightSide">
 
