a1e64d3195d22ec6cea66da17e7474202d30c190
Parth Tyagi <parthtyagicode@gmail.com>
2023-02-12 21:36:49 +0530

    checkout page completed

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -13,6 +13,8 @@ function CheckoutPage() {
 
             <div className="checkoutPageLeftSide">
 
+                {/* above part... */}
+
                 <div className="reviewAndShipping">
 
                     <h2>Review Item and Shipping</h2>
@@ -45,7 +47,7 @@ function CheckoutPage() {
 
                 </div>
 
-
+                {/* bottom part... */}
 
                 <div className="deliveryInformation">
 
@@ -65,12 +67,12 @@ function CheckoutPage() {
 
                     <div className="address customerInfo">
                         <label>Address *</label>
-                        <input type="text" name="address" />
+                        <input type="text" name="address" placeholder='Type here...' />
                     </div>
 
                     <div className="city">
                         <div class='customerInfo'>
-                            <label>City / Town</label>
+                            <label>City / Town *</label>
                             <input type="text" name="city" placeholder='Type here...' />
                         </div>
 
@@ -82,7 +84,7 @@ function CheckoutPage() {
 
                     <div className="customerId">
                         <div class='customerInfo'>
-                            <label>Mobile</label>
+                            <label>Mobile *</label>
                             <input type="text" name="mobile" placeholder='Type here...' />
                         </div>
 
@@ -102,6 +104,107 @@ function CheckoutPage() {
             <div className="checkoutPageRightSide">
 
                 <div className="orderSummary">
+
+                    <h2>Order Summary</h2>
+
+                    <div className="couponCode">
+                        <input type="text" name="coupon" placeholder='Enter Coupon Code' />
+                        <button>Apply Coupon</button>
+                    </div>
+
+                    <div className="paymentDetails">
+
+                        <h4>Payment Details</h4>
+
+                        <div className="paymentMethods">
+                            <div>
+                                <input type="radio" name='payment' value="cash" id='1' />
+                                <label htmlFor='1'>Cash on Delivery</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" name='payment' value="shopcart" id='2' />
+                                <label htmlFor='2'>Shopcart Card</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" name='payment' value="paypal" id='3' />
+                                <label htmlFor='3'>Paypal</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" name='payment' value="creditDebit" id='4' checked='true' />
+                                <label htmlFor='4'>Credit or Debit card</label>
+                            </div>
+
+                            <div className="cardImages">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Old_Visa_Logo.svg/2560px-Old_Visa_Logo.svg.png" alt="card_image" />
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/2560px-MasterCard_Logo.svg.png" alt="card_image" />
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/c/cb/Rupay-Logo.png" alt="card_image" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cardDetails">
+                        <div>
+                            <label>Email *</label>
+                            <input type="text" placeholder='Type here...' />
+                        </div>
+
+                        <div>
+                            <label>Card Holder Name *</label>
+                            <input type="text" placeholder='Type here...' />
+                        </div>
+
+                        <div>
+                            <label>Card Number *</label>
+                            <input type="text" placeholder='Type here...' />
+                        </div>
+
+                        <div>
+                            <div className="expiry">
+                                <label>Expiry *</label>
+                                <input type="text" placeholder='Type here...' />
+                            </div>
+
+                            <div className="cvc">
+                                <label>CVC *</label>
+                                <input type="text" placeholder='Type here...' />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="billingAmount">
+                        <div>
+                            <span>Sub Total</span>
+                            <span>$549</span>
+                        </div>
+
+                        <div>
+                            <span>Tax (10%)</span>
+                            <span>-$54</span>
+                        </div>
+
+                        <div>
+                            <span>Coupon Discount</span>
+                            <span>-$54</span>
+                        </div>
+
+                        <div>
+                            <span>Shipping Cost</span>
+                            <span>-$0</span>
+                        </div>
+
+                        <div>
+                            <span>Total</span>
+                            <span>=$449</span>
+                        </div>
+                    </div>
+
+                    <div className="orderPay">
+                        <button className="pay">Pay $449</button>
+                    </div>
 
                 </div>
 
