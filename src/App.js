b3c7f4db0eddb5a83c3d302f3fe7081685e004a8
Parth Tyagi <parthtyagicode@gmail.com>
2023-02-15 17:50:16 +0530

    routes setup

--- src/App.js
+++ src/App.js
@@ -6,22 +6,29 @@ import RecordBoxCart from './components/RecordBoxCart/RecordBoxCart';
 import SingleShoePage from './pages/SingleShowPage/SingleShoePage';
 import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
 import Homepage from './pages/Homepage/Homepage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Routes>
 
-      {/* <Navbar /> */}
-      {/* <Header /> */}
-      {/* <Main /> */}
-      <Homepage />
-      {/* <SingleShoePage /> */}
-      {/* <CheckoutPage /> */}
+        {/* <Navbar /> */}
+        {/* <Header /> */}
+        {/* <Main /> */}
+        {/* <Homepage /> */}
+        {/* <SingleShoePage /> */}
+        {/* <CheckoutPage /> */}
 
-    </div>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/shoe/:id' element={<SingleShoePage />} />
+        <Route path='/checkout' element={<CheckoutPage />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
